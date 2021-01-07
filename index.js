36aa8e692cae9008509561923cc1a8003dc65977
Mario Hernandez <mariohernandezk10@gmail.com>
2021-01-07 00:21:09 -0600

    added tables and created some psuedocode

---
+++ index.js
@@ -0,0 +1,77 @@
+var mysql = require("mysql2");
+var inquirer = require("inquirer");
+
+var connection = mysql.createConnection({
+    host: "localhost",
+    // Your port; if not 3306
+    port: 3306,
+    // Your username
+    user: "root",
+    // Your password
+    password: "password",
+    database: "employeeTracker_DB"
+});
+
+connection.connect(async function (err) {
+    if (err) throw err;
+    // run the start function after the connection is made to prompt the user
+    console.log('connected as id ' + connection.threadId);
+    await start();
+    connection.end();
+
+});
+
+// Functions to VIEW
+// 1. VIEW Departments
+// 2. VIEW Roles
+// 3. VIEW Employees
+async function viewEmployee() {
+    const SQL_STATEMENT = 'select * from department;';
+    const [rows, fields] = await connection.promise().query(SQL_STATEMENT);
+    console.table(rows);
+
+}
+
+async function viewRole() {
+    const SQL_STATEMENT = 'select * from role;';
+    const [rows, fields] = await connection.promise().query(SQL_STATEMENT);
+    console.table(rows);
+
+};
+
+// Functions to ADD
+// 1. ADD Departments
+// 2. ADD Roles
+// 3. ADD Employees
+async function start() {
+    const {
+        choice
+    } = await inquirer.prompt({
+        name: "choice",
+        type: "rawlist",
+        message: "What do you want to do?",
+        // put these in variables
+        choices: ["View Employee", "View Employee with manager id"]
+    })
+    console.log(choice);
+    switch (choice) {
+        // reference variables here
+        case "View Employee":
+            viewEmployee();
+            break;
+    }
+}
+
+// Functions to UPDATE
+// 1. UPDATE Roles
+// 2. UPDATE Employees
+
+
+
+
+//Bonus points if you're able to:
+// * Update employee managers
+// * View employees by manager
+// * Delete departments, roles, and employees
+// * View the total utilized budget of a department 
+// ie the combined salaries of all employees in that department
---
+++ schema.sql
@@ -0,0 +1,48 @@
+DROP DATABASE IF EXISTS employeeTracker_DB;
+CREATE DATABASE employeeTracker_DB;
+
+USE employeeTracker_DB;
+
+CREATE TABLE department(
+  id INT NOT NULL AUTO_INCREMENT,
+  name VARCHAR(30) NOT NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE role(
+  id INT NOT NULL AUTO_INCREMENT,
+  title VARCHAR(30) NOT NULL,
+  salary decimal,
+--   to hold reference to department role belongs to
+  department_id int,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE employee(
+  id INT NOT NULL AUTO_INCREMENT,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+--   INT to hold reference to role employee has
+  role_id int,
+-- to hold reference to another employee that 
+-- manages the employee being Created. This field 
+-- may be null if the employee has no manager
+  manager_id int,
+  PRIMARY KEY (id)
+);
+
+insert into department(name) values("Human Resources");
+insert into department(name) values("Developers");
+
+insert into role(title, salary, department_id) values("Counselor", 50000, 1);
+insert into role(title, salary, department_id) values("Payroll", 50000, 1);
+insert into role(title, salary, department_id) values("Full Stack Developer", 80000, 2);
+insert into role(title, salary, department_id) values("Project Manager", 100000, 2);
+
+insert into employee(first_name, last_name, role_id, manager_id) values("Mario", "Hernandez", 2, 1);
+insert into employee(first_name, last_name, role_id, manager_id) values("Ferd", "Dallas", 2, 1);
+insert into employee(first_name, last_name, role_id, manager_id) values("Chid", "Houston", 2, 1);
+insert into employee(first_name, last_name, role_id, manager_id) values("Austin", "Texas", 2, 1);
+insert into employee(first_name, last_name, role_id, manager_id) values("Drake", "Canada", 2, 1);
+insert into employee(first_name, last_name, role_id, manager_id) values("Curtis", "Jackson", 2, 1);
+
